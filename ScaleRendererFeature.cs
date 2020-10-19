1f9470839cd3ab0315d26226d1a9ce16012e2894
Jakub Bursztyński <48991565+Kyuubixi@users.noreply.github.com>
2020-10-19 17:09:46 +0200

    Added Scale Renderer Feature

---
+++ ScaleRendererFeature.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ScaleRendererFeature : ScriptableRendererFeature
+{
+    class CustomRenderPass : ScriptableRenderPass
+    {
+        public RenderTargetIdentifier source;
+
+        private int iterations;
+        private RenderTargetHandle tempRenderTargetHandle; // Temporary texture
+
+        public CustomRenderPass(int p_iterations)
+        {
+            iterations = p_iterations;
+
+            tempRenderTargetHandle.Init("_TemporaryColorTexture");
+        }
+        // This method is called before executing the render pass.
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in an performance manner.
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            float width = cameraTextureDescriptor.width;
+            float height = cameraTextureDescriptor.height;
+
+            for (int i = 1; i < iterations; i++)
+            {
+                width *= 0.5f;
+                height *= 0.5f;
+            }
+
+            cmd.GetTemporaryRT(tempRenderTargetHandle.id, (int)width, (int)height, 0);
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get("Downsample Render Pass");
+
+            Blit(cmd, source, tempRenderTargetHandle.Identifier());
+            Blit(cmd, tempRenderTargetHandle.Identifier(), source);
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        /// Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(tempRenderTargetHandle.id);
+        }
+    }
+
+    [System.Serializable]
+    public class Settings
+    {
+        [Range(1, 8)] public int iterations = 1;
+    }
+
+    public Settings settings = new Settings();
+
+    CustomRenderPass m_ScriptablePass;
+
+    public override void Create()
+    {
+        m_ScriptablePass = new CustomRenderPass(settings.iterations);
+
+        // Configures where the render pass should be injected.
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_ScriptablePass.source = renderer.cameraColorTarget;
+
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
+
+
